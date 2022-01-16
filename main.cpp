d67bee096bc261a44a4e663c1ed0a0b4ecf99af1
n0uben <benjamin.noureux@gmail.com>
2022-01-16 18:22:11 +0100

    ajout des includes et d’une fonction main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "Dictionnaires.h"
+#include "Documents.h"
+#include "Livres.h"
+#include "Manuels.h"
+#include "Romans.h"
+#include "StockDocuments.h"
+
+using namespace std;
+
+
+int main(void)
+{
+    cout << "hello world" << endl;
+}
